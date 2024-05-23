f0bcf1ed272c98f2ca1c0b6bd1099d4c06c76db9
Mahmoud <mahmoudfarid102@gmail.com>
2024-05-23 14:30:30 +0300

    "Version-1_FundMe"

---
+++ script/HelperConfig.s.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {Script} from "forge-std/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+
+contract HelperConfig is Script{
+    /* 
+        1. Deploy mocks when we are on a local anvil chain
+        2. Keep track of contract address across different chains
+    */
+
+    struct NetworkConfig {
+        address priceFeed;
+    }
+
+    NetworkConfig public activeNetworkConfig;
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else if (block.chainid == 1) {
+            activeNetworkConfig = getMainnetEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliaEthConfig() public pure returns (NetworkConfig memory) {
+        // Price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x694AA1769357215DE4FAC081bf1f309aDC325306
+        });
+        return sepoliaConfig;
+    }
+
+    function getMainnetEthConfig() public pure returns (NetworkConfig memory) {
+        // Price feed address
+        NetworkConfig memory ethConfig = NetworkConfig({
+            priceFeed: 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419
+        });
+        return ethConfig;
+    }
+
+    function getAnvilEthConfig() public returns (NetworkConfig memory) {
+        // Price feed address
+
+        // 1. Deploy the mocks
+        // 2. Return the mock address
+        vm.startBroadcast();
+
+        MockV3Aggregator mockPriceFeed = new MockV3Aggregator(8, 2000e8);
+
+        vm.stopBroadcast();
+
+        NetworkConfig memory anvilConfig = NetworkConfig({priceFeed: address(mockPriceFeed)});
+
+        return anvilConfig;
+    }
+
+}
---
+++ test/FundMeTest.t.sol
@@ -0,0 +1,31 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import {DeployFundMe} from "../script/DeployFundMe.s.sol";
+import {Test, console} from "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
+
+contract FundMeTest is Test {
+    FundMe fundMe;
+
+    // us -> FundMeTest -> FundMe
+    // i_owner = FundMeTest     not us
+
+    function setUp() external {
+        //fundMe = new FundMe(0x694AA1769357215DE4FAC081bf1f309aDC325306);
+        DeployFundMe deployFundMe = new DeployFundMe();
+        fundMe = deployFundMe.run();
+    }
+
+    function testMinimumDollarIsFive() public view {
+        assertEq(fundMe.MINIMUM_USD(), 5e18);
+    }
+
+    function testOwnerIsMsgSender() public view {
+        assertEq(fundMe.i_owner(), msg.sender);
+    }
+
+    function testPriceFeedVersionIsAccurate() public view {
+        assertEq(fundMe.getVersion(), 4);
+    }
+}
